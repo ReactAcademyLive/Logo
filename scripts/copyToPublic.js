3fb390c749a7ea7cbf76bc80dd26792f491f1651
Eric Cote <eric@coteexpert.com>
2025-01-22 10:41:15 -0500

    changes

--- scripts/copyToPublic.js
+++ scripts/copyToPublic.js
@@ -1,6 +1,7 @@
 import fs from 'node:fs';
 import os from 'node:os';
 import { resolve } from 'node:path';
+import process from 'node:process';
 
 const src = './model.gltf';
 const destPath = '../../public';
@@ -9,14 +10,14 @@ const srcPath = resolve(src);
 const dstPath = resolve(destPath, src);
 const logPath = resolve(os.homedir(), log);
 
-console.log(logPath);
+fs.appendFileSync(logPath, `Info: project path is ${process.cwd()}\n`);
 
-if (fs.existsSync(destPath)) {
-  fs.copyFileSync(srcPath, dstPath);
-  fs.appendFileSync(logPath, `Info: copied ${srcPath} to ${dstPath}`);
-} else {
-  fs.appendFileSync(
-    logPath,
-    `Public folder not found, cannot copy  ${srcPath} to ${dstPath}`
-  );
-}
+// if (fs.existsSync(destPath)) {
+//   fs.copyFileSync(srcPath, dstPath);
+//   fs.appendFileSync(logPath, `Info: copied ${srcPath} to ${dstPath}`);
+// } else {
+//   fs.appendFileSync(
+//     logPath,
+//     `Public folder not found, cannot copy  ${srcPath} to ${dstPath}`
+//   );
+// }
