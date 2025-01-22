e4eb056964270dd17f396e8a3c3e8ae4870b1375
Eric Cote <eric@coteexpert.com>
2025-01-22 10:32:38 -0500

    change script

--- scripts/copyToPublic.js
+++ scripts/copyToPublic.js
@@ -1,18 +1,21 @@
 import fs from 'node:fs';
+import os from 'node:os';
 import { resolve } from 'node:path';
 
 const src = './model.gltf';
 const destPath = '../../public';
-const log = './log.txt';
+const log = 'log.txt';
 const srcPath = resolve(src);
 const dstPath = resolve(destPath, src);
-const logPath = resolve(log);
+const logPath = resolve(os.homedir(), log);
+
+console.log(logPath);
 
 if (fs.existsSync(destPath)) {
   fs.copyFileSync(srcPath, dstPath);
-  fs.appendFile(logPath, `Info: copied ${srcPath} to ${dstPath}`);
+  fs.appendFileSync(logPath, `Info: copied ${srcPath} to ${dstPath}`);
 } else {
-  fs.appendFile(
+  fs.appendFileSync(
     logPath,
     `Public folder not found, cannot copy  ${srcPath} to ${dstPath}`
   );
